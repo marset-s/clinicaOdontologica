d3ce106364c0957d3bb41d96c6dc031945f6f877
Serrana Marset <serranamarset@gmail.com>
2023-07-10 20:29:49 -0300

    Primer commit

---
+++ src/main/java/com/clinicaodontologica/DrMuelitas/DrMuelitasApplication.java
@@ -0,0 +1,13 @@
+package com.clinicaodontologica.DrMuelitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrMuelitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrMuelitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicaodontologica/DrMuelitas/DrMuelitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicaodontologica.DrMuelitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrMuelitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
